9655889646f45d9830e710f9cee95c0300f3d332
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-17 22:27:12 +0700

    Перенес activity в presentation слой

--- app/src/main/java/com/elmirov/shiftlabtesttask/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/elmirov/shiftlabtesttask/presentation/activity/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.elmirov.shiftlabtesttask
+package com.elmirov.shiftlabtesttask.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.elmirov.shiftlabtesttask.R
+import com.elmirov.shiftlabtesttask.ShiftlabApplication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

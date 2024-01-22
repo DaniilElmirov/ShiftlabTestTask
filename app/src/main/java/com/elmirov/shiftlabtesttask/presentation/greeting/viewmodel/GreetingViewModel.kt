3291fccc79c6c22124b8863cac76aad3ac4847d7
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-22 17:17:56 +0700

    Refactor. Заменил репозиторий на юзкейс

--- app/src/main/java/com/elmirov/shiftlabtesttask/presentation/greeting/viewmodel/GreetingViewModel.kt
+++ app/src/main/java/com/elmirov/shiftlabtesttask/presentation/greeting/viewmodel/GreetingViewModel.kt
@@ -1,21 +1,21 @@
 package com.elmirov.shiftlabtesttask.presentation.greeting.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.elmirov.shiftlabtesttask.domain.repository.SessionRepository
+import com.elmirov.shiftlabtesttask.domain.usecase.GetUserNameUseCase
 import com.elmirov.shiftlabtesttask.presentation.greeting.state.GreetingState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class GreetingViewModel @Inject constructor(
-    private val sessionRepository: SessionRepository,
+    private val getUserNameUseCase: GetUserNameUseCase,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<GreetingState>(GreetingState.Initial)
     val state = _state.asStateFlow()
 
     fun greeting() {
-        val name = sessionRepository.getName() ?: "NO REGISTERED"
+        val name = getUserNameUseCase() ?: "NO REGISTERED" //TODO переделать
         _state.value = GreetingState.Content(name)
     }
 
